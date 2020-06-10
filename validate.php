20a9a595c7c051b7b1748c608a61c7d11e7a7043
RMidhunSuresh <rmidhunsuresh@gmail.com>
2020-06-10 11:55:51 +0530

    Final Commit

--- validate.php
+++ validate.php
@@ -1,4 +1,10 @@
 <?php
+/* Submitted by Group 11
+   R Midhun Suresh
+   Rebin Shaji
+   Nihal Narayan
+   Nilammudeen
+*/
 $result = preg_match("/([0-9]{3}-){2}[0-9]{4}$/", $_POST["phone"]) ? "correct" : "wrong";
 print "Phone number is $result";
 ?>
